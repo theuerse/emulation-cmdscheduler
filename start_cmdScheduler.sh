997c6e4cbbeb8ec5472636dfa62304e9d82c1fe5
theuerse <theuers@posteo.de>
2018-08-13 14:26:57 +0200

    small adjustments to build, gitignore and usage example

--- start_cmdScheduler.sh
+++ start_cmdScheduler.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-./build/cmdScheduler examples/example.cmd 2017-07-17 07:19:00.000
-./build/cmdScheduler examples/example.cmd 2017-07-17 07:19:00.000 & sudo chrt -r -p 50 $!
+./build/cmdScheduler examples/example.cmd
+#./build/cmdScheduler examples/example.cmd 2017-07-17 07:19:00.000
+#./build/cmdScheduler examples/example.cmd 2017-07-17 07:19:00.000 & sudo chrt -r -p 50 $!
